3f763197fc761cb28a76b99dd744b95d8f154085
Mohamed Ragab <Ragab@mohameds-macbook-pro.local>
2016-08-02 11:06:04 +0200

    Initial Commit

---
+++ FoodPin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoodPin
+//
+//  Created by Mohamed Ragab on 8/2/16.
+//  Copyright © 2016 Mohamed Ragab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
